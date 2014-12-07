956f3c69083011c82543713289adb2208c6311c5
phannguyen <pcnguyen1985@gmail.com>
2014-12-07 18:03:46 +0700

    create mvp structure for project

---
+++ EasyCare/app/src/main/java/vn/easycare/layers/services/IWebServiceAccess.java
@@ -0,0 +1,13 @@
+package vn.easycare.layers.services;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by phannguyen on 12/7/14.
+ */
+
+public interface IWebServiceAccess<T extends IWebServiceModel> {
+    static final String WEBSERVICE_HOST = "https://easycare.vn/api/";
+
+}
---
+++ EasyCare/app/src/main/java/vn/easycare/layers/services/IWebServiceModel.java
@@ -0,0 +1,7 @@
+package vn.easycare.layers.services;
+
+/**
+ * Created by phannguyen on 12/7/14.
+ */
+public interface IWebServiceModel {
+}
