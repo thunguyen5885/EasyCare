1f997a3f9c72fdc6176c88af78952188621fd6d6
thunguyen5885 <thunguyen5885@gmail.com>
2014-12-17 16:08:31 +0700

    Dating list screen

--- EasyCare/app/src/main/java/vn/easycare/layers/ui/components/views/DatingListLayout.java
+++ EasyCare/app/src/main/java/vn/easycare/layers/ui/components/views/DatingListLayout.java
@@ -1,14 +1,18 @@
 package vn.easycare.layers.ui.components.views;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Calendar;
 
 import vn.easycare.R;
 import vn.easycare.layers.ui.components.adapters.DatingListAdapter;
@@ -27,6 +31,11 @@ public class DatingListLayout extends LinearLayout{
     private View mSearchLayout;
     private View mLoadMoreView;
 
+    // For data, object
+    private int mSelectedYear;
+    private int mSelectedMonth;
+    private int mSelectedDay;
+
     private LayoutInflater mLayoutInflater;
     public DatingListLayout(Context context) {
         super(context);
@@ -43,6 +52,10 @@ public class DatingListLayout extends LinearLayout{
         init(context);
     }
     private void init(Context context) {
+        mSelectedYear = -1;
+        mSelectedMonth = -1;
+        mSelectedDay = -1;
+
         mLayoutInflater = LayoutInflater.from(context);
         View view = mLayoutInflater.inflate(R.layout.dating_pager_item_ctrl, null);
         mLoadMoreView = mLayoutInflater.inflate(R.layout.load_more_ctrl, null);
@@ -73,7 +86,7 @@ public class DatingListLayout extends LinearLayout{
         public void onClick(View v) {
             switch (v.getId()){
                 case R.id.rlSelectCalendarLayout:
-                    Toast.makeText(getContext(), "Select calendar", Toast.LENGTH_SHORT).show();
+                    showDatePickerDialog();
                     break;
                 case R.id.datingListSearchLayout:
                     Toast.makeText(getContext(), "Search clicked", Toast.LENGTH_SHORT).show();
@@ -81,4 +94,39 @@ public class DatingListLayout extends LinearLayout{
             }
         }
     };
+    private DatePickerDialog.OnDateSetListener mOnDateSetListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            mSelectedYear = year;
+            mSelectedMonth = monthOfYear;
+            mSelectedDay = dayOfMonth;
+
+            // Update on UI
+            mTvCalendarText.setText(mSelectedDay + "/" + (mSelectedMonth + 1) + "/" + mSelectedYear);
+        }
+    };
+    private void showDatePickerDialog(){
+        Calendar calendar = Calendar.getInstance();
+        int yearToSet = 0;
+        int monthToSet = 0;
+        int dayToSet = 0;
+        if(mSelectedYear != -1){
+            yearToSet = mSelectedYear;
+        }else{
+            yearToSet = calendar.get(Calendar.YEAR);
+        }
+        if(mSelectedMonth != -1){
+            monthToSet = mSelectedMonth;
+        }else{
+            monthToSet = calendar.get(Calendar.MONTH);
+        }
+        if(mSelectedDay != -1){
+            dayToSet = mSelectedDay;
+        }else{
+            dayToSet = calendar.get(Calendar.DAY_OF_MONTH);
+        }
+        DatePickerDialog datePickerDialog = new DatePickerDialog(getContext(), mOnDateSetListener, yearToSet, monthToSet, dayToSet);
+        datePickerDialog.show();
+    }
+
 }
