f952aef0a4065e700ee1fbae750142b32b1204af
thunguyen5885 <thunguyen5885@gmail.com>
2015-01-16 16:52:17 +0700

    Change to real server

--- EasyCare/app/src/main/java/vn/easycare/layers/services/IWebServiceAccess.java
+++ EasyCare/app/src/main/java/vn/easycare/layers/services/IWebServiceAccess.java
@@ -13,7 +13,8 @@ import java.util.Map;
  */
 
 public interface IWebServiceAccess<T extends IWebServiceModel,P extends IWebServiceParamModel> {
-    static final String WEBSERVICE_HOST = "http://edev.easycare.vn/api/v1";
+    //static final String WEBSERVICE_HOST = "http://edev.easycare.vn/api/v1";
+    static final String WEBSERVICE_HOST = "http://easycare.vn/api/v1";
     String getWSURL();
     Map<String,String> getWSParams();
     void setWSParams(IWebServiceParamModel params);
