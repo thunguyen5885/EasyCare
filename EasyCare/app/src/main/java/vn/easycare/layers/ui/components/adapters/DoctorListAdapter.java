2a9eed2cce1c447d294f63319fe8a734fdfd0596
thunguyen5885 <thunguyen5885@gmail.com>
2015-03-20 17:44:45 +0700

    doctor list screen

---
+++ EasyCare/app/src/main/java/vn/easycare/layers/ui/components/adapters/DoctorListAdapter.java
@@ -0,0 +1,161 @@
+package vn.easycare.layers.ui.components.adapters;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.android.volley.toolbox.NetworkImageView;
+
+import java.util.List;
+
+import vn.easycare.R;
+import vn.easycare.layers.ui.activities.HomeActivity;
+import vn.easycare.layers.ui.components.data.PatientManagementItemData;
+import vn.easycare.layers.ui.components.singleton.DataSingleton;
+import vn.easycare.layers.ui.fragments.AppointmentListForAPatientFragment;
+import vn.easycare.utils.AppConstants;
+import vn.easycare.utils.AppFnUtils;
+
+/**
+ * Created by ThuNguyen on 12/16/2014.
+ */
+public class DoctorListAdapter extends BaseAdapter{
+    public interface IDoctorListClickListener {
+        public void goToAppointmentPage(String doctorId);
+        public void goToCalendarPage(String doctorId);
+    }
+    private Context mContext;
+    private LayoutInflater mLayoutInflater;
+    private boolean mIsClicked = false;
+    private boolean mIsEndOfList = false;
+    private IDoctorListClickListener mDoctorListClickListener;
+    private List<PatientManagementItemData> mItemDataList;
+
+    public DoctorListAdapter(Context context){
+        mContext = context;
+        mLayoutInflater = LayoutInflater.from(context);
+    }
+    public void setItemDataList(List<PatientManagementItemData> itemDataList){
+        mItemDataList = itemDataList;
+    }
+    public void setDoctorListClickListener(IDoctorListClickListener doctorListClickListener){
+        mDoctorListClickListener = doctorListClickListener;
+    }
+    public void setIsEndOfList(boolean isEndOfList){
+        mIsEndOfList = isEndOfList;
+    }
+    @Override
+    public int getCount() {
+        return (mItemDataList != null) ? mItemDataList.size() : 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder;
+        if(convertView == null){
+            convertView = mLayoutInflater.inflate(R.layout.doctor_item_ctrl, null);
+            viewHolder = new ViewHolder();
+            viewHolder.mDoctorAvatar = (NetworkImageView) convertView.findViewById(R.id.doctorAvatarThumb);
+            viewHolder.mDoctorName = (TextView) convertView.findViewById(R.id.tvDoctorName);
+            viewHolder.mDoctorPhone = (TextView) convertView.findViewById(R.id.tvDoctorPhone);
+            viewHolder.mDoctorEmail = (TextView) convertView.findViewById(R.id.tvDoctorEmail);
+            viewHolder.mTvAppointment = (TextView) convertView.findViewById(R.id.tvAppointmentForDoctor);
+            viewHolder.mTvCalendar = (TextView) convertView.findViewById(R.id.tvCalendarForDoctor);
+            viewHolder.mBottomIndicator = convertView.findViewById(R.id.bottomIndicator);
+            viewHolder.mEndOfListIndicator = convertView.findViewById(R.id.endOfListIndicator);
+            convertView.setTag(viewHolder);
+        }else{
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        if(position == getCount() - 1 && mIsEndOfList){
+            viewHolder.mBottomIndicator.setVisibility(View.GONE);
+            viewHolder.mEndOfListIndicator.setVisibility(View.VISIBLE);
+        }else{
+            viewHolder.mBottomIndicator.setVisibility(View.VISIBLE);
+            viewHolder.mEndOfListIndicator.setVisibility(View.GONE);
+        }
+        // Set on click
+        viewHolder.mTvAppointment.setTag(position);
+        viewHolder.mTvAppointment.setOnClickListener(mOnClickListener);
+        viewHolder.mTvCalendar.setTag(position);
+        viewHolder.mTvCalendar.setOnClickListener(mOnClickListener);
+
+        // Calculate the avatar size
+        int screenWidth = AppFnUtils.getScreenWidth((Activity)mContext);
+        int avatarSize = screenWidth / 5;
+        viewHolder.mDoctorAvatar.getLayoutParams().width = avatarSize;
+        viewHolder.mDoctorAvatar.getLayoutParams().height = avatarSize;
+
+        // Set fake data
+        PatientManagementItemData itemData = mItemDataList.get(position);
+        viewHolder.mDoctorAvatar.setDefaultImageResId(R.drawable.ic_no_avatar);
+        viewHolder.mDoctorAvatar.setImageUrl(itemData.getPatientAvatar(), DataSingleton.getInstance(mContext).getImageLoader());
+        viewHolder.mDoctorName.setText(itemData.getPatientName());
+        if(itemData.getPatientEmailAddress() != null && itemData.getPatientEmailAddress().length() > 0) {
+            viewHolder.mDoctorEmail.setText(itemData.getPatientEmailAddress());
+            viewHolder.mDoctorEmail.setTextColor(mContext.getResources().getColor(R.color.textview_color_default));
+        }else{
+            viewHolder.mDoctorEmail.setText(R.string.nothing_data);
+            viewHolder.mDoctorEmail.setTextColor(mContext.getResources().getColor(R.color.textview_color_grey));
+        }
+        if(itemData.getPatientPhoneNumber() != null && itemData.getPatientPhoneNumber().length() > 0) {
+            viewHolder.mDoctorPhone.setText(itemData.getPatientPhoneNumber());
+            viewHolder.mDoctorPhone.setTextColor(mContext.getResources().getColor(R.color.textview_color_default));
+        }else{
+            viewHolder.mDoctorPhone.setText(R.string.nothing_data);
+            viewHolder.mDoctorPhone.setTextColor(mContext.getResources().getColor(R.color.textview_color_grey));
+        }
+
+        // Apply font
+        AppFnUtils.applyFontForTextViewChild(convertView);
+        return convertView;
+    }
+    private View.OnClickListener mOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if(mIsClicked){
+                return;
+            }
+            mIsClicked = true;
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mIsClicked = false;
+                }
+            }, 500);
+            switch (v.getId()){
+                case R.id.tvAppointmentForDoctor:
+                    break;
+                case R.id.tvCalendarForDoctor:
+                    break;
+            }
+        }
+    };
+    private static class ViewHolder{
+        private NetworkImageView mDoctorAvatar;
+        private TextView mDoctorName;
+        private TextView mDoctorPhone;
+        private TextView mDoctorEmail;
+        private TextView mTvAppointment;
+        private TextView mTvCalendar;
+        private View mBottomIndicator;
+        private View mEndOfListIndicator;
+    }
+}
---
+++ EasyCare/app/src/main/java/vn/easycare/layers/ui/fragments/DoctorListFragment.java
@@ -0,0 +1,203 @@
+package vn.easycare.layers.ui.fragments;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import vn.easycare.R;
+import vn.easycare.layers.ui.activities.HomeActivity;
+import vn.easycare.layers.ui.base.BaseFragment;
+import vn.easycare.layers.ui.components.adapters.DoctorListAdapter;
+import vn.easycare.layers.ui.components.data.PatientManagementItemData;
+import vn.easycare.layers.ui.components.views.LoadMoreLayout;
+import vn.easycare.utils.AppFnUtils;
+import vn.easycare.utils.DialogUtil;
+
+/**
+ * Created by ThuNguyen on 12/13/2014.
+ */
+public class DoctorListFragment extends BaseFragment{
+    private int mTotalItemCount = 0;
+
+    // For control, layout
+    private ProgressBar mPbLoading;
+    private ListView mCommentListView;
+    private TextView mTvNoData;
+    private DoctorListAdapter mDoctorAdapter;
+    private LoadMoreLayout mLoadMoreView;
+    private View mRefreshLayout;
+    private Dialog mLoadingDialog;
+
+    // For data, object
+    private List<PatientManagementItemData> mCommentAndAssessmentItemDatas;
+    //private ICommentAndAssessmentPresenter mPresenter;
+    private int mPage;
+    public DoctorListFragment(){
+
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mCommentAndAssessmentItemDatas = new ArrayList<PatientManagementItemData>();
+        //mPresenter = new CommentAndAssessmentPresenterImpl(this, getActivity());
+        mPage = 1;
+        View v = inflater.inflate(R.layout.fragment_doctor_list, container, false);
+        mPbLoading = (ProgressBar) v.findViewById(R.id.pbLoading);
+        mRefreshLayout = v.findViewById(R.id.refreshLayout);
+        mRefreshLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                refreshData();
+            }
+        });
+        mCommentListView = (ListView) v.findViewById(R.id.doctorListView);
+        mTvNoData = (TextView) v.findViewById(R.id.tvNoData);
+        mLoadMoreView = new LoadMoreLayout(getActivity());
+        mLoadMoreView.setOnLoadMoreClickListener(mOnLoadMoreClickListener);
+        mCommentListView.addFooterView(mLoadMoreView);
+
+        // Apply font
+        AppFnUtils.applyFontForTextViewChild(v);
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        loadNewData();
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if(getActivity() != null){
+            ((HomeActivity) getActivity()).showFooterSeparator();
+            ((HomeActivity) getActivity()).showHeaderBackButton();
+        }
+    }
+
+    private void loadMoreData(){
+        mPage ++;
+        mLoadMoreView.beginLoading();
+        loadData();
+    }
+    private void loadNewData(){
+        mPage = 1;
+        mCommentAndAssessmentItemDatas.clear();
+        mPbLoading.setVisibility(View.VISIBLE);
+        mCommentListView.setVisibility(View.GONE);
+        loadData();
+    }
+    private void refreshData(){
+        mPage = 1;
+        mCommentAndAssessmentItemDatas.clear();
+        mLoadingDialog = DialogUtil.createLoadingDialog(getActivity(), getString(R.string.loading_dialog_in_progress));
+        mLoadingDialog.show();
+        loadData();
+    }
+    /**
+     * Reload data to get the newest data after enable "Not-display"
+     */
+    private void reloadData(){
+        mPage = 1;
+        mCommentAndAssessmentItemDatas.clear();
+        loadData();
+    }
+    private void loadData(){
+        //mPresenter.loadCommentAndAssessmentForDoctor( mPage);
+    }
+    private void updateUI(){
+        mPbLoading.setVisibility(View.GONE);
+        mCommentListView.setVisibility(View.VISIBLE);
+        if(mLoadingDialog != null){
+            mLoadingDialog.dismiss();
+        }
+        if(mDoctorAdapter == null || mPage == 1){
+            mDoctorAdapter = new DoctorListAdapter(getActivity());
+            mDoctorAdapter.setItemDataList(mCommentAndAssessmentItemDatas);
+            mCommentListView.setAdapter(mDoctorAdapter);
+        }else{
+            mDoctorAdapter.notifyDataSetChanged();
+        }
+        if(mCommentAndAssessmentItemDatas.size() == 0){ // No data
+            mTvNoData.setVisibility(View.VISIBLE);
+        }else{
+            mTvNoData.setVisibility(View.GONE);
+        }
+    }
+    //@Override
+    public void DisplayAllCommentAndAssessmentForDoctor(List<PatientManagementItemData> commentAndAssessmentItemsList) {
+
+        if(commentAndAssessmentItemsList != null && commentAndAssessmentItemsList.size() > 0){
+            if(mPage == 1){ // Load for first time
+                if(mCommentAndAssessmentItemDatas != null){
+                    mCommentAndAssessmentItemDatas.clear();
+                }
+                mTotalItemCount = commentAndAssessmentItemsList.get(0).getTotalItems();
+                mCommentAndAssessmentItemDatas.addAll(commentAndAssessmentItemsList);
+            }else{ // Load more here
+                mCommentAndAssessmentItemDatas.addAll(commentAndAssessmentItemsList);
+            }
+            // Decide to hide load more or not
+            if(mCommentAndAssessmentItemDatas.size() == mTotalItemCount){ // End of list
+                mLoadMoreView.closeView();
+                mCommentListView.removeFooterView(mLoadMoreView);
+            }else {
+                mCommentListView.removeFooterView(mLoadMoreView);
+                mLoadMoreView.loadMoreComplete();
+                mCommentListView.addFooterView(mLoadMoreView);
+            }
+        }else{ // Maybe failed or data is end of list
+            mLoadMoreView.closeView();
+            mCommentListView.removeFooterView(mLoadMoreView);
+        }
+        // Update UI anyway
+        updateUI();
+    }
+
+    //@Override
+    public void DisplayMessageForHideCommentAndAssessment(String message) {
+        boolean mIsUpdatedDone = true;
+        if(mIsUpdatedDone) {
+            // Load new data
+            reloadData();
+        }else{
+            if(mLoadingDialog != null){
+                mLoadingDialog.dismiss();
+            }
+        }
+    }
+
+    //@Override
+    public void DisplayMessageIncaseError(String message,String funcTitle) {
+
+        DialogUtil.createInformDialog(this.getActivity(), funcTitle, message,
+                new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                }).show();
+    }
+
+    private LoadMoreLayout.ILoadMoreClickListener mOnLoadMoreClickListener = new LoadMoreLayout.ILoadMoreClickListener() {
+        @Override
+        public void onLoadMoreClicked() {
+            loadMoreData();
+        }
+    };
+}
